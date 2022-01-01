bad78b2fb0b2bd7b397f3de86b2d79d054803973
Saulo Costa <saulotarsobc@gmail.com>
2022-01-01 14:51:58 -0300

    ADD SCRIPT

--- asname.sh
+++ asname.sh
@@ -1,24 +1,3 @@
-# Material de apoio
-
-[Bash split string into array using 4 simple methods
-](https://www.golinuxcloud.com/bash-split-string-into-array-linux/)
-
-[JSONPath Online Evaluator - jsonpath.com
-](http://jsonpath.com/)
-
-[JSON Formatter](https://jsonformatter.curiousconcept.com/)
-
-[Bash For Loop Array: Iterate Through Array Values](https://www.cyberciti.biz/faq/bash-for-loop-array/)
-
-[How to use bash array in a shell script](https://linuxconfig.org/how-to-use-arrays-in-bash-script)
-
-[remontti / Zabbix-Templates](https://github.com/remontti/Zabbix-Templates/tree/main/Huawei/BGP)
-
-[LAÇOS OU LOOPS COM SHELL SCRIPT (FOR, WHILE E UNTI](https://www.livrosdelinux.com.br/lacos-ou-loops-for-while-e-until/)
-
-## Script
-
-```sh
 #! /usr/bin/bash
 # 1 - comunidade snmp
 # 2 - ip do dispositivo
@@ -46,4 +25,3 @@ while [  $C -lt $MAX ]; do
     let C=C+1;
 done
 echo ']';
-```
