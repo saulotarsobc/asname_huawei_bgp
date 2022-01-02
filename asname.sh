4d6555e4f867e01f685674bd2a244be07b5a7827
Saulo Costa <saulotarsobc@gmail.com>
2022-01-01 22:38:31 -0300

    ADD REMOTE ADDRES
    ADD REMOTE ADDRES NO RETORNO DO JSON

--- asname.sh
+++ asname.sh
@@ -2,26 +2,34 @@
 # 1 - comunidade snmp
 # 2 - ip do dispositivo
 
-#  ASNAME
-## SNMPWALK NO OID QUE RETORNA OS ASNAME
-ASNAMES=$(snmpwalk -v 2c -c $1 $2 | cut  -d ":" -f 2 | sed -e "s/^[ \t]*//");
+# ASNAME ## SNMPWALK NO OID QUE RETORNA OS ASNAME
+ASNAMES=$(snmpwalk -v 2c -c $1 $2 1.3.6.1.4.1.2011.5.25.177.1.1.2.1.2  | sed 's/.*: //');
 ASNAMES=$(echo $ASNAMES);
 read -a ARRASNAMES <<< $ASNAMES;
 
-#  SNMPINDEX
-## SNMPWALK NO OID QUE RETORNA OS IPS >> CORRIGIR
-SNMPINDEXS=$(snmpwalk -v 2c -c 10l15p130A@huawei 45.7.118.1 1.3.6.1.2.1.15.3.1.7 | cut  -d ":" -f 2 | sed -e "s/^[ \t]*//");
+# SNMPINDEX ## SNMPWALK NO OID QUE RETORNA OS INDEX SNMP
+SNMPINDEXS=$(snmpwalk -v 2c -c $1 $2 1.3.6.1.4.1.2011.5.25.177.1.1.2.1.2 | sed 's/ = .*//'  | sed 's/iso.3.6.1.4.1.2011.5.25.177.1.1.2.1.2.//');
 SNMPINDEXS=$(echo $SNMPINDEXS);
 read -a ARRSNMPINDEXS <<< $SNMPINDEXS;
+
+# REMOTEADD ## SNMPWALK NO OID QUE RETORNA OS REMOTE ADDRESS
+REMOTEADDS=$(snmpwalk -v 2c -c $1 $2 1.3.6.1.4.1.2011.5.25.177.1.1.2.1.4 | sed 's/.*: //' | sed 's/"//' | sed 's/"//');
+REMOTEADDS=$(echo $REMOTEADDS);
+read -a ARRREMOTEADDS <<< $REMOTEADDS;
 
 C=0; # CONTADOR
 MAX=${#ARRASNAMES[@]}; # QUANTIDADE DE ITENS NO ARRAY
 
-echo '[';
+echo '[{"BGP": [';
 while [  $C -lt $MAX ]; do
+
     ASNAME=$(whois -h whois.cymru.com  AS${ARRASNAMES[$C]} | egrep -v "AS Name");
     SNMPINDEX=${ARRSNMPINDEXS[$C]};
-    echo '{"ASNAME":"'$ASNAME'","SNMPINDEX":"'$SNMPINDEX'"},'
+    REMOTEADD=${ARRREMOTEADDS[$C]};
+
+    echo '{"ASNAME":"'$ASNAME'","SNMPINDEX":"'$SNMPINDEX'","REMOTEADD":"'$REMOTEADD'"},'
+
     let C=C+1;
+
 done
-echo ']';
+echo ']}]'
